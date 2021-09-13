99260dbb9f842bcbc1dcb4115f7abbbd8a64b569
edmondcheung <cwh.edmond@gmail.com>
2021-09-12 21:22:58 -0700

    Remove style6 animation and update selected fontWeight

--- lib/nav-bar-styles/style-6-bottom-nav-bar.widget.dart
+++ lib/nav-bar-styles/style-6-bottom-nav-bar.widget.dart
@@ -12,11 +12,7 @@ class BottomNavStyle6 extends StatefulWidget {
   _BottomNavStyle6State createState() => _BottomNavStyle6State();
 }
 
-class _BottomNavStyle6State extends State<BottomNavStyle6>
-    with TickerProviderStateMixin {
-  late List<AnimationController> _animationControllerList;
-  late List<Animation<double>> _animationList;
-
+class _BottomNavStyle6State extends State<BottomNavStyle6> with TickerProviderStateMixin {
   int? _lastSelectedIndex;
   int? _selectedIndex;
 
@@ -25,94 +21,64 @@ class _BottomNavStyle6State extends State<BottomNavStyle6>
     super.initState();
     _lastSelectedIndex = 0;
     _selectedIndex = 0;
-    _animationControllerList = List<AnimationController>.empty(growable: true);
-    _animationList = List<Animation<double>>.empty(growable: true);
-
-    for (int i = 0; i < widget.navBarEssentials!.items!.length; ++i) {
-      _animationControllerList.add(AnimationController(
-          duration:
-              widget.navBarEssentials!.itemAnimationProperties?.duration ??
-                  Duration(milliseconds: 400),
-          vsync: this));
-      _animationList.add(Tween(begin: 0.95, end: 1.18)
-          .chain(CurveTween(
-              curve: widget.navBarEssentials!.itemAnimationProperties?.curve ??
-                  Curves.ease))
-          .animate(_animationControllerList[i]));
-    }
-
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      _animationControllerList[_selectedIndex!].forward();
-    });
   }
 
-  Widget _buildItem(PersistentBottomNavBarItem item, bool isSelected,
-      double? height, int itemIndex) {
+  Widget _buildItem(PersistentBottomNavBarItem item, bool isSelected, double? height, int itemIndex) {
     return widget.navBarEssentials!.navBarHeight == 0
         ? SizedBox.shrink()
-        : AnimatedBuilder(
-            animation: _animationList[itemIndex],
-            builder: (context, child) => Transform.scale(
-              scale: _animationList[itemIndex].value,
-              child: Container(
-                width: 150.0,
-                height: height,
-                child: Container(
-                  alignment: Alignment.center,
-                  height: height,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Expanded(
-                        child: IconTheme(
-                          data: IconThemeData(
-                              size: item.iconSize,
-                              color: isSelected
-                                  ? (item.activeColorSecondary == null
-                                      ? item.activeColorPrimary
-                                      : item.activeColorSecondary)
-                                  : item.inactiveColorPrimary == null
-                                      ? item.activeColorPrimary
-                                      : item.inactiveColorPrimary),
-                          child: isSelected
-                              ? item.icon
-                              : item.inactiveIcon ?? item.icon,
-                        ),
-                      ),
-                      item.title == null
-                          ? SizedBox.shrink()
-                          : Padding(
-                              padding: const EdgeInsets.only(top: 15.0),
-                              child: Material(
-                                type: MaterialType.transparency,
-                                child: FittedBox(
-                                  child: Text(
-                                    item.title!,
-                                    style: item.textStyle != null
-                                        ? (item.textStyle!.apply(
-                                            color: isSelected
-                                                ? (item.activeColorSecondary ==
-                                                        null
-                                                    ? item.activeColorPrimary
-                                                    : item.activeColorSecondary)
-                                                : item.inactiveColorPrimary))
-                                        : TextStyle(
-                                            color: isSelected
-                                                ? (item.activeColorSecondary ==
-                                                        null
-                                                    ? item.activeColorPrimary
-                                                    : item.activeColorSecondary)
-                                                : item.inactiveColorPrimary,
-                                            fontWeight: FontWeight.w400,
-                                            fontSize: 12.0),
-                                  ),
-                                ),
+        : Container(
+            width: 150.0,
+            height: height,
+            child: Container(
+              alignment: Alignment.center,
+              height: height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: IconTheme(
+                      data: IconThemeData(
+                          size: item.iconSize,
+                          color: isSelected
+                              ? (item.activeColorSecondary == null
+                                  ? item.activeColorPrimary
+                                  : item.activeColorSecondary)
+                              : item.inactiveColorPrimary == null
+                                  ? item.activeColorPrimary
+                                  : item.inactiveColorPrimary),
+                      child: isSelected ? item.icon : item.inactiveIcon ?? item.icon,
+                    ),
+                  ),
+                  item.title == null
+                      ? SizedBox.shrink()
+                      : Padding(
+                          padding: const EdgeInsets.only(top: 15.0),
+                          child: Material(
+                            type: MaterialType.transparency,
+                            child: FittedBox(
+                              child: Text(
+                                item.title!,
+                                style: item.textStyle != null
+                                    ? (item.textStyle!.apply(
+                                        color: isSelected
+                                            ? (item.activeColorSecondary == null
+                                                ? item.activeColorPrimary
+                                                : item.activeColorSecondary)
+                                            : item.inactiveColorPrimary))
+                                    : TextStyle(
+                                        color: isSelected
+                                            ? (item.activeColorSecondary == null
+                                                ? item.activeColorPrimary
+                                                : item.activeColorSecondary)
+                                            : item.inactiveColorPrimary,
+                                        fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+                                        fontSize: 12.0),
                               ),
                             ),
-                    ],
-                  ),
-                ),
+                          ),
+                        ),
+                ],
               ),
             ),
           );
@@ -120,52 +86,23 @@ class _BottomNavStyle6State extends State<BottomNavStyle6>
 
   @override
   void dispose() {
-    for (int i = 0; i < widget.navBarEssentials!.items!.length; ++i) {
-      _animationControllerList[i].dispose();
-    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (widget.navBarEssentials!.items!.length !=
-        _animationControllerList.length) {
-      _animationControllerList =
-          List<AnimationController>.empty(growable: true);
-      _animationList = List<Animation<double>>.empty(growable: true);
-
-      for (int i = 0; i < widget.navBarEssentials!.items!.length; ++i) {
-        _animationControllerList.add(AnimationController(
-            duration:
-                widget.navBarEssentials!.itemAnimationProperties?.duration ??
-                    Duration(milliseconds: 400),
-            vsync: this));
-        _animationList.add(Tween(begin: 0.95, end: 1.18)
-            .chain(CurveTween(
-                curve:
-                    widget.navBarEssentials!.itemAnimationProperties?.curve ??
-                        Curves.ease))
-            .animate(_animationControllerList[i]));
-      }
-    }
     if (widget.navBarEssentials!.selectedIndex != _selectedIndex) {
       _lastSelectedIndex = _selectedIndex;
       _selectedIndex = widget.navBarEssentials!.selectedIndex;
-      _animationControllerList[_selectedIndex!].forward();
-      _animationControllerList[_lastSelectedIndex!].reverse();
     }
     return Container(
       width: double.infinity,
       height: widget.navBarEssentials!.navBarHeight,
       padding: EdgeInsets.only(
-          left: widget.navBarEssentials!.padding?.left ??
-              MediaQuery.of(context).size.width * 0.04,
-          right: widget.navBarEssentials!.padding?.right ??
-              MediaQuery.of(context).size.width * 0.04,
-          top: widget.navBarEssentials!.padding?.top ??
-              widget.navBarEssentials!.navBarHeight! * 0.15,
-          bottom: widget.navBarEssentials!.padding?.bottom ??
-              widget.navBarEssentials!.navBarHeight! * 0.12),
+          left: widget.navBarEssentials!.padding?.left ?? MediaQuery.of(context).size.width * 0.04,
+          right: widget.navBarEssentials!.padding?.right ?? MediaQuery.of(context).size.width * 0.04,
+          top: widget.navBarEssentials!.padding?.top ?? widget.navBarEssentials!.navBarHeight! * 0.15,
+          bottom: widget.navBarEssentials!.padding?.bottom ?? widget.navBarEssentials!.navBarHeight! * 0.12),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -175,25 +112,20 @@ class _BottomNavStyle6State extends State<BottomNavStyle6>
             child: GestureDetector(
               onTap: () {
                 if (widget.navBarEssentials!.items![index].onPressed != null) {
-                  widget.navBarEssentials!.items![index].onPressed!(
-                      widget.navBarEssentials!.selectedScreenBuildContext);
+                  widget
+                      .navBarEssentials!.items![index].onPressed!(widget.navBarEssentials!.selectedScreenBuildContext);
                 } else {
                   if (index != _selectedIndex) {
                     _lastSelectedIndex = _selectedIndex;
                     _selectedIndex = index;
-                    _animationControllerList[_selectedIndex!].forward();
-                    _animationControllerList[_lastSelectedIndex!].reverse();
                   }
                   widget.navBarEssentials!.onItemSelected!(index);
                 }
               },
               child: Container(
                 color: Colors.transparent,
-                child: _buildItem(
-                    item,
-                    widget.navBarEssentials!.selectedIndex == index,
-                    widget.navBarEssentials!.navBarHeight,
-                    index),
+                child: _buildItem(item, widget.navBarEssentials!.selectedIndex == index,
+                    widget.navBarEssentials!.navBarHeight, index),
               ),
             ),
           );
